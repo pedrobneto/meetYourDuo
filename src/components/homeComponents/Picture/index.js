1695badf44248592923e28260be8113a11780b8b
Pedro Neto <pedropaivabneto@gmail.com>
2020-05-07 19:06:36 -0300

    Finishing home screen design

--- src/components/homeComponents/Picture/index.js
+++ src/components/homeComponents/Picture/index.js
@@ -2,15 +2,20 @@ import React from "react";
 import {
     View,
     Text,
+    Image,
 } from "react-native";
 
 import styles from './styles'
 
+const IMAGE_URL = 'https://br.web.img3.acsta.net/pictures/17/02/06/17/01/343859.jpg'
+
 const Picture = (props) => (
     <View style={styles.containerStyle}>
-        <Text>
-            Picture
-        </Text>
+        <Image
+            style={styles.imageStyle}
+            resizeMode='cover'
+            source={{ uri: IMAGE_URL }}
+        />
     </View>
 )
 
--- src/components/homeComponents/Picture/styles.js
+++ src/components/homeComponents/Picture/styles.js
@@ -7,9 +7,16 @@ const styles = StyleSheet.create({
         flex: 8,
         backgroundColor: 'white',
         width: windowWidth - 10,
-        borderRadius: 5,
+        borderRadius: 10,
         elevation: 2,
+        overflow: 'hidden'
     },
+
+    imageStyle: {
+        height: undefined,
+        width: undefined,
+        flex: 1,
+    }
 })
 
 export default styles;
